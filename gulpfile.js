4764f8f3cd28622468bee19b5d8860d8f6ebefa5
Thomas Boko <bokothomas@yahoo.fr>
2020-10-12 16:20:55 +0100

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,7 @@ var webpack = require('webpack-stream');
 var fs = require('fs');
 
 
-gulp.task('build', ['build-client', 'build-server', 'test']);
 
-gulp.task('test', ['lint'], function () {
-    gulp.src(['test/**/*.js'])
-        .pipe(mocha());
-});
 
 gulp.task('lint', function () {
   return gulp.src(['**/*.js', '!node_modules/**/*.js', '!bin/**/*.js'])
@@ -26,7 +21,18 @@ gulp.task('lint', function () {
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('build-client', ['lint', 'move-client'], function () {
+gulp.task('test', gulp.series('lint'), function () {
+    gulp.src(['test/**/*.js'])
+        .pipe(mocha());
+});
+
+gulp.task('move-client', function () {
+    return gulp.src(['src/client/**/*.*', '!client/js/*.js'])
+        .pipe(gulp.dest('./bin/client/'));
+});
+
+
+gulp.task('build-client', gulp.series('lint', 'move-client'), function () {
   return gulp.src(['src/client/js/app.js'])
     .pipe(uglify())
     .pipe(webpack(require('./webpack.config.js')))
@@ -38,31 +44,28 @@ gulp.task('build-client', ['lint', 'move-client'], function () {
     .pipe(gulp.dest('bin/client/js/'));
 });
 
-gulp.task('move-client', function () {
-  return gulp.src(['src/client/**/*.*', '!client/js/*.js'])
-    .pipe(gulp.dest('./bin/client/'));
-});
 
-
-gulp.task('build-server', ['lint'], function () {
+gulp.task('build-server', gulp.series('lint'), function () {
   return gulp.src(['src/server/**/*.*', 'src/server/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('bin/server/'));
 });
 
-gulp.task('watch', ['build'], function () {
+gulp.task('build', gulp.series('build-client', 'build-server', 'test'));
+
+gulp.task('watch', gulp.series('build'), function () {
   gulp.watch(['src/client/**/*.*'], ['build-client', 'move-client']);
   gulp.watch(['src/server/*.*', 'src/server/**/*.js'], ['build-server']);
   gulp.start('run-only');
 });
 
-gulp.task('todo', ['lint'], function() {
+gulp.task('todo', gulp.series('lint'), function() {
   gulp.src('src/**/*.js')
       .pipe(todo())
       .pipe(gulp.dest('./'));
 });
 
-gulp.task('run', ['build'], function () {
+gulp.task('run', gulp.series('build'), function () {
     nodemon({
         delay: 10,
         script: './server/server.js',
@@ -88,4 +91,4 @@ gulp.task('run-only', function () {
     });
 });
 
-gulp.task('default', ['run']);
+gulp.task('default', gulp.series('run'));
